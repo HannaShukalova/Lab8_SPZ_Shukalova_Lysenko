ac2e7fc1d97b60ae11322cdea6b39d53c8c64a4e
HannaShukalova <HannaShukalova@DESKTOP-F74TLGF>
2021-11-29 12:06:26 +0200

    Добавьте файлы проекта.

---
+++ Lab8_team1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace Lab8_team1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetRegistryData();
+        }
+
+        public void GetRegistryData()
+        {
+            RegistryKey keyBIOS = Registry.LocalMachine.OpenSubKey("HARDWARE").OpenSubKey("DESCRIPTION").OpenSubKey("SYSTEM").OpenSubKey("BIOS");
+            lbBiosVersion.Content = "BIOS Version: " + keyBIOS?.GetValue("BIOSVersion")?.ToString();
+            lbBiosVendor.Content = "BIOS Vendor: " + keyBIOS?.GetValue("BIOSVendor")?.ToString();
+            keyBIOS.Close();
+
+            RegistryKey keyCPU = Registry.LocalMachine.OpenSubKey("HARDWARE").OpenSubKey("DESCRIPTION").OpenSubKey("SYSTEM").
+                OpenSubKey("CentralProcessor").OpenSubKey("0");
+            lbBiosCpuType.Content = "CPU type: " + keyCPU?.GetValue("Identifier")?.ToString();
+            keyCPU.Close();
+        }
+    }
+}
